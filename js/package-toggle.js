2ce77f788c860253655a76229dd3c7f5417a8a48
Kirill Lavrov <lkv@myttk.ru>
2021-06-04 15:52:55 +0700

    create: added interactivity for the other-packages

---
+++ js/package-toggle.js
@@ -0,0 +1,41 @@
+const childOf = (childElem, className) => {
+  while(!childElem.classList.contains(className)) {
+    childElem = childElem.parentNode 
+    if (childElem === document.body) return false
+  }
+  return childElem; 
+}
+
+document.addEventListener("click", (event) => {
+
+  if (window.innerWidth >= 767 ) {
+    
+    const activePackageCards = childOf(event.target, 'packag-card');
+    
+    if (!childOf(event.target, 'full') && activePackageCards) {
+
+      if (!event.target.classList.contains('packag-card__button')) {  
+    
+        const fullElems = document.querySelectorAll('.full');
+     
+        fullElems.forEach((elem) => {
+          elem.classList.remove('full')
+        });
+
+        // .packag-card
+        activePackageCards.classList.add('full')
+        // .packag-card__img
+        activePackageCards.children[0].classList.add('full')
+        // .packag-card__flash-offer
+        activePackageCards.children[1].classList.add('full')
+        // .packag-card__booking-info
+        activePackageCards.children[2].classList.add('full')
+        // .packag-card__rating-bar
+        activePackageCards.children[2].children[0].classList.add('full')
+        // .packag-card__text
+        activePackageCards.children[2].children[2].classList.add('full')
+      }
+    }
+  }
+});
+
