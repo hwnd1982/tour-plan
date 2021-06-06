2622783ce9e53685184db50e40cd727332a41971
Kirill Lavrov <lkv@myttk.ru>
2021-06-06 22:22:38 +0700

    create: added animation for m.menu & scroll lock

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -4,11 +4,24 @@ const mobileMenuNavbarMenu = document.querySelector('.navbar-menu')
 const mobileMenuButton = document.querySelector('.menu-button')
 
 document.addEventListener("click", (event) => { 
-  console.log('event.target: ', event.target);
   if (event.target.classList.contains('menu-button') || event.target.classList.contains('menu-button__line')) {
     mobileMenuSearch.classList.toggle('hidden')
     mobileMenuUser.classList.toggle('hidden')
     mobileMenuNavbarMenu.classList.toggle('hidden')
+    mobileMenuButton.classList.toggle('active')
+    let scrollBarWidth = window.visualViewport.width
     document.body.classList.toggle('lock')
+    scrollBarWidth = window.visualViewport.width - scrollBarWidth
+    const navbarTop = document.querySelector('.navbar-top')
+    if (scrollBarWidth > 0 ) {
+      document.body.style.setProperty('margin-right', scrollBarWidth + 'px')
+      navbarTop.style.setProperty('padding-right', scrollBarWidth + 'px')
+      mobileMenuNavbarMenu.style.setProperty('padding-right', scrollBarWidth + 'px')
+    }
+    else {
+      document.body.style.setProperty('margin-right', 0)
+      navbarTop.style.setProperty('padding-right', 0)
+      mobileMenuNavbarMenu.setProperty('padding-right', 0)
+    }
   }
 });
