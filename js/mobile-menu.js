bcc653097b80b6c9d0d7d10d0a1ab62a34fc0788
Kirill Lavrov <lkv@myttk.ru>
2021-06-11 21:14:29 +0700

    fix: corrections made

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -1,27 +1,35 @@
-const mobileMenuSearch = document.querySelector('.search')
-const mobileMenuUser = document.querySelector('.user')
+const mobileMenuSearch = document.querySelector('.navbar-menu__search')
+const mobileMenuUser = document.querySelector('.navbar-menu__user')
 const mobileMenuNavbarMenu = document.querySelector('.navbar-menu')
 const mobileMenuButton = document.querySelector('.menu-button')
 
 document.addEventListener("click", (event) => { 
-  if (event.target.classList.contains('menu-button') || event.target.classList.contains('menu-button__line')) {
+  if (event.target.classList.contains('menu-button') ||
+      event.target.classList.contains('menu-button__line') ||
+      event.target.classList.contains('navbar-menu__link')) {
+    
     mobileMenuSearch.classList.toggle('hidden')
     mobileMenuUser.classList.toggle('hidden')
     mobileMenuNavbarMenu.classList.toggle('hidden')
     mobileMenuButton.classList.toggle('active')
-    let scrollBarWidth = window.visualViewport.width
-    document.body.classList.toggle('lock')
-    scrollBarWidth = window.visualViewport.width - scrollBarWidth
-    const navbarTop = document.querySelector('.navbar-top')
-    if (scrollBarWidth > 0 ) {
-      document.body.style.setProperty('margin-right', scrollBarWidth + 'px')
-      navbarTop.style.setProperty('padding-right', scrollBarWidth + 'px')
-      mobileMenuNavbarMenu.style.setProperty('padding-right', scrollBarWidth + 'px')
-    }
-    else {
-      document.body.style.setProperty('margin-right', 0)
-      navbarTop.style.setProperty('padding-right', 0)
-      mobileMenuNavbarMenu.setProperty('padding-right', 0)
+    
+    if (window.innerWidth <= 767) {
+      let scrollBarWidth = window.visualViewport.width
+      document.body.classList.toggle('lock')
+      scrollBarWidth = window.visualViewport.width - scrollBarWidth
+      const navbarTop = document.querySelector('.navbar-top')
+      
+
+      if (scrollBarWidth > 0 ) {
+        document.body.style.setProperty('margin-right', scrollBarWidth + 'px')
+        navbarTop.style.setProperty('padding-right', scrollBarWidth + 'px')
+        mobileMenuNavbarMenu.firstElementChild.style.setProperty('padding-right', scrollBarWidth + 'px')
+      }
+      else {
+        document.body.style.setProperty('margin-right', 0)
+        navbarTop.style.setProperty('padding-right', 0)
+        mobileMenuNavbarMenu.firstElementChild.style.setProperty('padding-right', 0)
+      }
     }
   }
 });
