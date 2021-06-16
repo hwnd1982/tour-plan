ef01db820524f026d56befd37e048d5fa24592ec
Kirill Lavrov <lkv@myttk.ru>
2021-06-17 01:11:38 +0700

    create: forms validation by jquery.validate

--- js/modal.js
+++ js/modal.js
@@ -16,7 +16,6 @@ document.addEventListener("click", (event) => {
     }
   }
   if (childOf(event.target, 'modal__button-close') ||
-      event.target.classList.contains('modal-form__button') ||
       event.target.classList.contains('modal__overlay')) {
     modalDialog.classList.add('hidden')
     document.body.classList.remove('lock')
@@ -26,6 +25,58 @@ document.addEventListener("click", (event) => {
       navbarTop.style.setProperty('padding-right', 0)
     }
   }
+  // Обработка форм
+  $('.form').each( function () {
+    if ($(this).hasClass("subscribe"))
+      $(this).validate({
+        errorLabelContainer: $('.subscribe__invalid-container'),
+        errorClass: "invalid",
+        rules: {
+          email: {
+            required: true,
+            email: true
+          }
+        },
+        messages: {
+          search: "Please enter your requestd",
+          email: {
+            required: "We need your email address to contact you",
+            email: "Your email address must be in the format of name@domain.com"
+          }
+        }
+      })
+    else 
+      $(this).validate({
+      errorClass: "invalid",
+      
+      rules: {
+        email: {
+          required: true,
+          email: true
+        }
+      },
+
+      messages: {
+        name: {
+          required: "Please specify your name",
+          minlength: "Name length cannot be less than 2 letters"
+        },
+        phone: {
+          required: "We need your phone number to contact you",
+          minlength: "Your phone number must be in the format of +7 (999) 999-99-99"
+        },
+        email: {
+          required: "We need your email address to contact you",
+          email: "Your email address must be in the format of name@domain.com"
+        }
+      }
+    })
+  })
+
+  $(document).ready(function(){
+    $('.phone_us').mask('+7 (000) 000-00-00');
+  })
+
 });
 
 document.addEventListener('keydown', (event) => {
