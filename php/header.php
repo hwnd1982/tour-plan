5879d0a97172199e7a42591873c805d6173ef2f2
Kirill Lavrov <lkv@myttk.ru>
2021-06-21 21:36:01 +0700

    fix: connecting fonts

--- php/header.php
+++ php/header.php
@@ -5,7 +5,11 @@
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
+  <link
+    href="https://fonts.googleapis.com/css2?family=Mulish:wght@300;400;600;700&family=Nunito:wght@400;600;800&display=swap"
+    rel="preload"
+    as="font"
+  >
   <link rel="stylesheet" href="css/style.css">
   <script src="js/mobile-menu.js" defer></script>
   <script src="js/modal.js" defer></script>
