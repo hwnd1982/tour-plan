01a50c86bb26a36ee1bf8aae5b7418cf6af99775
Kirill Lavrov <lkv@myttk.ru>
2021-06-22 21:02:22 +0700

    fix: google maps container

--- js/modal.js
+++ js/modal.js
@@ -1,6 +1,7 @@
 const modalDialog = document.querySelector('.modal');
 
 const childOf = (childElem, className) => {
+  if (childElem === document.documentElement) return false
   while(!childElem.classList.contains(className)) {
     childElem = childElem.parentNode 
     if (childElem === document.body) return false
